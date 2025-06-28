6c5b11446cebad748fede1c199399418b3b66e35
Vincent Siliakus <vincent@usendes.com>
2025-06-28 17:12:47 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -7,9 +7,35 @@ defmodule Vaux.MixProject do
       version: "0.3.3",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: [{:jsv, "~> 0.8"}, {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}]
+      deps: deps(),
+      name: "Vaux",
+      source_url: "https://github.com/zambal/vaux",
+      description: "Composable HTML for Elixir",
+      docs: docs(),
+      package: package()
     ]
   end
 
   def application, do: []
+
+  defp deps do
+    [
+      {:jsv, "~> 0.8"},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp docs do
+    [main: "readme", extras: ["README.md"]]
+  end
+
+  defp package do
+    [
+      name: :vaux,
+      maintainers: ["Vincent Siliakus"],
+      licenses: ["Apache-2.0"],
+      links: %{"Github" => "https://github.com/zambal/vaux"}
+    ]
+  end
 end
