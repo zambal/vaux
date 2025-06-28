4ee2d94ab95c3a812110b6337c0c4ffbe76a16df
Vincent Siliakus <vincent@usendes.com>
2025-06-28 15:42:47 +0200

    Fix github workflow elixir/otp versions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Vaux.MixProject do
     [
       app: :vaux,
       version: "0.3.3",
-      elixir: "~> 1.17",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: [{:jsv, "~> 0.8"}]
     ]
