e8aff46783d50420282e2932192d94f9f00dd803
Vincent Siliakus <vincent@usendes.com>
2025-06-28 15:50:11 +0200

    Add Dialixir as dev and test dependency

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Vaux.MixProject do
       version: "0.3.3",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: [{:jsv, "~> 0.8"}]
+      deps: [{:jsv, "~> 0.8"}, {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}]
     ]
   end
 
